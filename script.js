de8ecb348d324f8936eafe34f2425f5911335cc2
jbrer <129300519+jbrer@users.noreply.github.com>
2025-02-09 13:06:12 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Добавление товара в корзину
-document.querySelectorAll('.add-to-cart').forEach(button => {
-	button.addEventListener('click', function () {
-		alert('Товар добавлен в корзину!')
-	})
-})
