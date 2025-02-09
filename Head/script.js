2f1aa6e4bf920bbf4cd2231aff2369d67cd3cb2a
jbrer <129300519+jbrer@users.noreply.github.com>
2025-02-09 13:06:56 +0300

    Add files via upload

---
+++ Head/script.js
@@ -0,0 +1,6 @@
+// Добавление товара в корзину
+document.querySelectorAll('.add-to-cart').forEach(button => {
+	button.addEventListener('click', function () {
+		alert('Товар добавлен в корзину!')
+	})
+})
